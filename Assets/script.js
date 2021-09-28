5d10557eaf0bc45ebd87ddf0c8349022bd73bdb1
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-28 15:59:59 -0600

    Created user City input

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,11 @@
 var APIKey = "efbd70459a14a736bd41d17e5170f50b";
 var city;
+var userInput = document.getElementById('city-name');
+var searchBtn = document.getElementById('city-search');
+
+function runWeather (){
+    userCity = userInput.value;
+    console.log(userCity);
+}
+
+searchBtn.addEventListener('click', runWeather);
