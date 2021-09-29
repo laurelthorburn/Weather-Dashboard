c2222c266c3cedf79db0b6d9092367f66655eca0
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-29 12:26:30 -0600

    Creating fetch function

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,28 @@
-var APIKey = "efbd70459a14a736bd41d17e5170f50b";
-var city;
+var APIKey = "&appid=efbd70459a14a736bd41d17e5170f50b";
 var userInput = document.getElementById('city-name');
 var searchBtn = document.getElementById('city-search');
+var userCity;
+var starterUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+var unitMeasurement = "&units=imperial";
+var finalUrl;
 
-function runWeather (){
-    userCity = userInput.value;
-    console.log(userCity);
+function submitSearch(e){
+    userCity = userInput.value; //works
+    fetchResults(e);
 }
 
-searchBtn.addEventListener('click', runWeather);
+function fetchResults(e) {
+ // Use preventDefault() to stop the form submitting
+ e.preventDefault();
+
+ // Assemble the full URL
+ finalUrl = starterUrl + userCity + APIKey + unitMeasurement;
+
+ console.log(finalUrl); //confirmed works
+}
+
+
+searchBtn.addEventListener('click', submitSearch);
+
+
+//need to have the link that uses different variables throughout it, like API key and input city
