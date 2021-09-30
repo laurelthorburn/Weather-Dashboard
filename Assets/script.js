c60d91204dfcdbf91c95ff1b1ac892468dadbaa6
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-30 08:27:59 -0600

    Setting up local storage

--- Assets/script.js
+++ Assets/script.js
@@ -9,10 +9,10 @@ var finalUrl;
 //using moment to get date
 //Setting current time as a global function
 var currentDate = moment().format("MMM Do YY");
-console.log(currentDate);
 
 function submitSearch(e){
     userCity = userInput.value; //works
+    saveSearch();
     getAPI(e);
 }
 
@@ -21,22 +21,33 @@ function getAPI(e) {
     e.preventDefault();
  // Assemble the full URL
     finalUrl = starterUrl + userCity + APIKey + unitMeasurement;
+
     fetch(finalUrl)
     .then(function (response) {
     return response.json();
     })
     .then(function (data) {// Use the console to examine the response
-    console.log(data);
     document.getElementById('city-display-name').innerHTML = userCity + " (" + currentDate + ")";
     document.getElementById('today-temp').innerHTML = data.main.temp;
     document.getElementById('today-wind').innerHTML = data.wind.speed + " MPH";
-    document.getElementById('today-humidity').innerHTML = data.main.humidity;
+    document.getElementById('today-humidity').innerHTML = data.main.humidity + "%";
     document.getElementById('today-uvi').innerHTML = data.main.uvi; //nope, doesn't work
     })
-    console.log(finalUrl); //confirmed works
     // now begin to display and style?
 
 };
+
+function saveSearch(){
+    //first save to local storage, then get from local storage then pass through append to show in page with dynamically created html
+    userCity;
+    console.log(userCity); //check that has same input as above, works!
+    localStorage.setItem('City', JSON.stringify(userCity));
+    let cityBtn = JSON.parse(localStorage.getItem('City'));
+
+    for (var i = 0; i< cityBtn.length; i++){
+    $('ul').append('<li class="list-group-item"><button class ="city-button" data-city = "' + userCity + '">'+ userCity + '</button></li>') //do i need to add the /n.. probs    
+    }
+}
 
 
 searchBtn.addEventListener('click', submitSearch);
