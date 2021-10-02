27cf89ae1eb27b211208ba425b29dd19fb6b897b
Laurel Thorburn <laurelthorburn@gmail.com>
2021-10-02 09:48:44 -0600

    Fixed issue where future dates would not change after first click

--- Assets/script.js
+++ Assets/script.js
@@ -6,30 +6,30 @@ var starterUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 var unitMeasurement = "&units=imperial";
 var finalUrl;
 var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var fiveDayFinalUrl;
 
-var clicked = false;
 
 //using moment to get date
 //Setting current time as a global function
 var currentDate = moment().format('l');
-var future1 =  moment().add(1, 'days').format('l');
-var future2 =  moment().add(2, 'days').format('l');
-var future3 =  moment().add(3, 'days').format('l');
-var future4 =  moment().add(4, 'days').format('l');
-var future5 =  moment().add(5, 'days').format('l');
+var future0 =  moment().add(1, 'days').format('l');
+var future1 =  moment().add(2, 'days').format('l');
+var future2 =  moment().add(3, 'days').format('l');
+var future3 =  moment().add(4, 'days').format('l');
+var future4 =  moment().add(5, 'days').format('l');
+
 
 function submitSearch(e){
     userCity = userInput.value; //works
     saveSearch();
-    getAPI(e);
+    getAPIcurrent(e);
 }
 
-function getAPI(e) {
+function getAPIcurrent(e) {
  // Use preventDefault() to stop the form submitting
     e.preventDefault();
  // Assemble the full URL
     finalUrl = starterUrl + userCity + APIKey + unitMeasurement;
-    console.log(finalUrl)
 
     fetch(finalUrl)
     .then(function (response) {
@@ -43,10 +43,18 @@ function getAPI(e) {
     document.getElementById('today-wind').innerHTML = data.wind.speed + " MPH";
     document.getElementById('today-humidity').innerHTML = data.main.humidity + "%";
     document.getElementById('today-uvi').innerHTML = data.main.uvi; //nope, doesn't work
-   
+    getAPIfuture(e);
 })
 
+};
+
+function getAPIfuture(e){
+    e.preventDefault();
+    console.log(userCity);
+
     fiveDayFinalUrl = fiveDayUrl + userCity + APIKey + unitMeasurement;
+
+    console.log(fiveDayFinalUrl);
 
     fetch(fiveDayFinalUrl)
     .then(function (response) {
@@ -57,32 +65,33 @@ function getAPI(e) {
         //5 day blue boxes
         $('#five-day-forecast').replaceWith(`<div class="card forecast-mini-card" style="width: 10rem;"><div class="card-body"><h5 class="card-title" id = "future-date0">Date Here</h5><p>Temp: <span id="future-temp0"></span></p><p>Wind: <span id="future-wind0"></span></p><p>Humidity: <span id="future-humidity0"></span></p></div></div><div class="card forecast-mini-card" style="width: 10rem;"><div class="card-body"><h5 class="card-title" id = "future-date1">Date Here</h5><p>Temp: <span id="future-temp1"></span></p><p>Wind: <span id="future-wind1"></span></p><p>Humidity: <span id="future-humidity1"></span></p></div></div><div class="card forecast-mini-card" style="width: 10rem;"><div class="card-body"><h5 class="card-title" id = "future-date2">Date Here</h5><p>Temp: <span id="future-temp2"></span></p><p>Wind: <span id="future-wind2"></span></p><p>Humidity: <span id="future-humidity2"></span></p></div></div><div class="card forecast-mini-card" style="width: 10rem;"><div class="card-body"><h5 class="card-title" id = "future-date3">Date Here</h5><p>Temp: <span id="future-temp3"></span></p><p>Wind: <span id="future-wind3"></span></p><p>Humidity: <span id="future-humidity3"></span></p></div></div><div class="card forecast-mini-card" style="width: 10rem;"><div class="card-body"><h5 class="card-title" id = "future-date4">Date Here</h5><p>Temp: <span id="future-temp4"></span></p><p>Wind: <span id="future-wind4"></span></p><p>Humidity: <span id="future-humidity4"></span></p></div></div>`)
 
-//Future Day 1
-            document.getElementById('future-date0').replaceWith(future1); 
-            document.getElementById('future-temp0').innerHTML =data.list[0].main.temp; 
-            document.getElementById('future-wind0').innerHTML = data.list[0].wind.speed + " MPH"; 
-            document.getElementById('future-humidity0').innerHTML = data.list[0].main.humidity + "%"; 
+// //Future Day 1
+            $('#future-date0').text(future0); 
+            $('#future-temp0').text(data.list[0].main.temp); 
+            $('#future-wind0').text(data.list[0].wind.speed + " MPH"); 
+            $('#future-humidity0').text(data.list[0].main.humidity + "%"); 
 //Future Day 2
-            document.getElementById('future-date1').replaceWith(future2); 
-            document.getElementById('future-temp1').replaceWith(data.list[1].main.temp); 
-            document.getElementById('future-wind1').replaceWith(data.list[1].wind.speed + " MPH"); 
-            document.getElementById('future-humidity1').replaceWith(data.list[1].main.humidity + "%"); 
-//Future Day 1
-            document.getElementById('future-date2').replaceWith(future3); 
-            document.getElementById('future-temp2').replaceWith(data.list[2].main.temp); 
-            document.getElementById('future-wind2').replaceWith(data.list[2].wind.speed + " MPH"); 
-            document.getElementById('future-humidity2').replaceWith(data.list[2].main.humidity + "%"); 
-//Future Day 1
-            document.getElementById('future-date3').replaceWith(future4); 
-            document.getElementById('future-temp3').replaceWith(data.list[3].main.temp); 
-            document.getElementById('future-wind3').replaceWith(data.list[3].wind.speed + " MPH"); 
-            document.getElementById('future-humidity3').replaceWith(data.list[3].main.humidity + "%"); 
-//Future Day 1
-            document.getElementById('future-date4').replaceWith(future5); 
-            document.getElementById('future-temp4').replaceWith(data.list[4].main.temp); 
-            document.getElementById('future-wind4').replaceWith(data.list[4].wind.speed + " MPH"); 
-            document.getElementById('future-humidity4').replaceWith(data.list[4].main.humidity + "%"); 
-})};
+            $('#future-date1').text(future1); 
+            $('#future-temp1').text(data.list[1].main.temp); 
+            $('#future-wind1').text(data.list[1].wind.speed + " MPH"); 
+            $('#future-humidity1').text(data.list[1].main.humidity + "%"); 
+//Future Day 3
+            $('#future-date2').text(future2); 
+            $('#future-temp2').text(data.list[2].main.temp); 
+            $('#future-wind2').text(data.list[2].wind.speed + " MPH"); 
+            $('#future-humidity2').text(data.list[2].main.humidity + "%"); 
+//Future Day 4
+            $('#future-date3').text(future3); 
+            $('#future-temp3').text(data.list[3].main.temp); 
+            $('#future-wind3').text(data.list[3].wind.speed + " MPH"); 
+            $('#future-humidity3').text(data.list[3].main.humidity + "%"); 
+//Future Day 5
+            $('#future-date4').text(future4); 
+            $('#future-temp4').text(data.list[4].main.temp); 
+            $('#future-wind4').text(data.list[4].wind.speed + " MPH"); 
+            $('#future-humidity4').text(data.list[4].main.humidity + "%"); 
+})
+};
 
 function saveSearch() {
     var savedCities = JSON.parse(localStorage.getItem("savedCities") || "[]");
@@ -93,15 +102,16 @@ function saveSearch() {
 
     // $('ul').html=""
     function displaySearch(){
-    $('ul').append('<li class="list-group-item"><button class ="city-button" data-city = "' + savedCities[savedCities.length-1] + '">'+ savedCities[savedCities.length-1] + '</button></li>') 
-};
+        $('ul').append('<li class="list-group-item"><button class ="city-button" data-city = "' + savedCities[savedCities.length-1] + '">'+ savedCities[savedCities.length-1] + '</button></li>');
+    };
 
 }
 
-
-
+function parentClick (){
+    
+}
 
 searchBtn.addEventListener('click', submitSearch);
-
+$('#search-parent').on('click', parentClick);
 
 //need to have the link that uses different variables throughout it, like API key and input city
